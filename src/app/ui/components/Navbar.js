b6a0307350e957091d83b174cf3e01bdd37a59a1
ostrich-egg <sauhardhakafle12@gmail.com>
2024-11-30 18:42:00 +0545

    Merging latest with earlier home page

--- src/app/ui/components/Navbar.js
+++ src/app/ui/components/Navbar.js
@@ -6,35 +6,32 @@ const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const pathname = usePathname();
   const isHomePage = pathname === '/';
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     // Calculate when we've scrolled past 70vh (hero section height)
+  //     const heroHeight = window.innerHeight * 0.7;
+  //     setIsScrolled(window.scrollY >= heroHeight);
+  //   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      // Calculate when we've scrolled past 70vh (hero section height)
-      const heroHeight = window.innerHeight * 0.7;
-      setIsScrolled(window.scrollY >= heroHeight);
-    };
+  //   if (isHomePage) {
+  //     window.addEventListener('scroll', handleScroll);
+  //   }
 
-    if (isHomePage) {
-      window.addEventListener('scroll', handleScroll);
-    }
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, [isHomePage]);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [isHomePage]);
-
-  const navbarClasses = `
-    fixed top-0 w-full z-50 
-    transition-colors duration-300 
-    ${isHomePage && !isScrolled ? 'bg-transparent backdrop-blur-sm' : 'bg-slate-950'} 
-    text-white flex flex-col md:flex-row justify-between items-center 
-    px-4 py-2 md:py-0 h-auto md:h-[8vh] space-y-2 md:space-y-0
-  `;
 
   return (
-    <nav className={navbarClasses}>
+    <nav className="
+    fixed  w-full z-50  
+    bg-slate-950
+    text-white flex flex-col md:flex-row justify-between items-center 
+    px-4 py-2 md:py-0 h-auto md:h-[8vh] space-y-2 md:space-y-0
+  ">
       This is Navbar
-    </nav>
+    </nav >
   );
 }
 
